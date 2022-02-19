e2c334a4841a337d2f7cda37d864aff3fe1f07ac
merinjo90 <merinjo90@gmail.com>
2022-02-19 11:49:48 +0530

    Part2-  Connect Django App to MYSQL Database     • python manage.py startapp login     • XAMPP open         ◦ ~/Downloads         ◦ cd /opt/lampp/         ◦ /opt/lampp$ sudo ./manager-linux-x64.run         ◦ sudo service apache2 stop         ◦ service mysql stop     • open phpmyadmin - http://localhost/phpmyadmin     • create new database – DB name tutorial     • connect MYSQL         ◦ python manage.py makemigrations         ◦ pip install mysqlclient         ◦ python3 manage.py migrate

--- 1_Register/register/register/register/settings.py
+++ 1_Register/register/register/register/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'login',
 ]
 
 MIDDLEWARE = [
@@ -74,9 +75,23 @@ WSGI_APPLICATION = 'register.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'tutorial',
+    #     'USER':'root',
+    #     'PASSWORD':'',
+    #     'HOST':'localhost',
+    #     'PORT':'3306'
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tutorial',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
