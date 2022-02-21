1d1bcee0b4f42a44263f676008d7f39ad9a2aa24
merinjo90 <merinjo90@gmail.com>
2022-02-21 17:15:37 +0530

    blog

--- 2_BLOG/Blog/blog/home/urls.py
+++ 2_BLOG/Blog/blog/home/urls.py
@@ -1,5 +1,7 @@
+from django.views.generic import RedirectView
 from django.urls import path
 from .views import *
 urlpatterns = [
-
+    path('',home,name='base.html'),
+    path(r'^favicon\.ico$',RedirectView.as_view(url='/static/blogs/favicon.ico')),
 ]
