42ebcd740abbf48c59854060a347f4f4ac365aae
merinjo90 <merinjo90@gmail.com>
2022-02-25 11:20:42 +0530

    media files add and get method apply view file

--- 2_Mysite/mysite/mysite/blog/models.py
+++ 2_Mysite/mysite/mysite/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    head=models.CharField(max_length=200)
+    description=models.CharField(max_length=200)
+    image=models.ImageField(upload_to='pics')
+
+    def __str__(self):
+        return self.head
--- 2_Mysite/mysite/mysite/blog/views.py
+++ 2_Mysite/mysite/mysite/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
 def home(request):
-    return render(request,'index.html')
+    obj=Post.objects.get(id=1)
+    return render(request,'index.html',{'obj':obj})
