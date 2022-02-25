2aee4c9c5cc0736a597e8b2926443701b4b50bbb
merinjo90 <merinjo90@gmail.com>
2022-02-25 12:00:21 +0530

    Passing dynamic data to django templates

--- 2_Mysite/mysite/mysite/blog/views.py
+++ 2_Mysite/mysite/mysite/blog/views.py
@@ -4,5 +4,5 @@ from .models import Post
 # Create your views here.
 
 def home(request):
-    obj=Post.objects.get(id=1)
+    obj=Post.objects.all()
     return render(request,'index.html',{'obj':obj})
