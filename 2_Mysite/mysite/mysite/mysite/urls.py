b2c84dd1d635402bee39cce01c656817ffb1e51b
merinjo90 <merinjo90@gmail.com>
2022-02-25 10:13:16 +0530

    template index file setting

--- 2_Mysite/mysite/mysite/mysite/urls.py
+++ 2_Mysite/mysite/mysite/mysite/urls.py
@@ -18,6 +18,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include('app1.urls')),
+    # path('home/', include('app1.urls')),
+    path('',include('blog.urls')),
 
 ]
