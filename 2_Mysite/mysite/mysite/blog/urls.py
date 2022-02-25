18bba951de1908ecafd868d1b82a5a4cd13a64de
merinjo90 <merinjo90@gmail.com>
2022-02-25 15:08:43 +0530

    user authentication in django

--- 2_Mysite/mysite/mysite/blog/urls.py
+++ 2_Mysite/mysite/mysite/blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 # from blog import views
 from .views import *
 
+app_name='blog'
+
 urlpatterns=[
     path('',home,name='home'),
     path('login/',signup,name='login')
--- 2_Mysite/mysite/mysite/blog/views.py
+++ 2_Mysite/mysite/mysite/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Post
+from django.contrib.auth import authenticate,login,logout
 
 # Create your views here.
 
@@ -8,4 +9,14 @@ def home(request):
     return render(request,'index.html',{'obj':obj})
 
 def signup(request):
+    username=request.POST.get('username')
+    password=request.POST.get('password')
+    if request.method=='POST':
+        user=authenticate(username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('blog:home')
+        else:
+            return redirect('blog:login')
+
     return render(request,'login.html')
