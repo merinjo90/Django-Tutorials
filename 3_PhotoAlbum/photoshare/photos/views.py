8575a9ebdb00d89c11b5c8a82c711e601941436d
merinjo90 <merinjo90@gmail.com>
2022-02-28 19:05:20 +0530

    Add new image functionality

--- 3_PhotoAlbum/photoshare/photos/views.py
+++ 3_PhotoAlbum/photoshare/photos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import *
 
 # Create your views here.
@@ -12,9 +12,30 @@ def gallery(request):
 
 def viewPhoto(request,pk):
     photo=Photo.objects.get(id=pk)
-
     return render(request,'photos/photo.html',{'photo':photo})
 
 
 def addPhoto(request):
-    return render(request,'photos/add.html')
+    categories=Category.objects.all()
+
+    if request.method =='POST':
+        data=request.POST
+        image=request.FILES.get('image')
+
+        if data['category'] != 'none':
+            category=Category.objects.get(id=data['category'])
+        elif data['category_new'] !='':
+            category,created=Category.objects.get_or_create(name=data['category_new'])
+        else:
+            category=None
+        photo=Photo.objects.create(
+            category=category,
+            description=data['description'],
+            image=image
+        )
+
+        return redirect('gallery')
+
+
+    context={'categories':categories}
+    return render(request,'photos/add.html',context)
