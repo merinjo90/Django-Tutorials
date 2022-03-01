d8c11b21de6c8716571522e04899eed795d4bbbb
merinjo90 <merinjo90@gmail.com>
2022-03-01 10:20:34 +0530

    modify images

--- 3_PhotoAlbum/photoshare/photos/views.py
+++ 3_PhotoAlbum/photoshare/photos/views.py
@@ -9,13 +9,9 @@ def gallery(request):
         photos = Photo.objects.all()
     else:
         photos = Photo.objects.filter(category__name=category)
-
     categories = Category.objects.all()
-    # photos=Photo.objects.all()
-
     context={'categories':categories, 'photos':photos}
     return render(request,'photos/gallery.html',context)
-
 
 def viewPhoto(request,pk):
     photo=Photo.objects.get(id=pk)
