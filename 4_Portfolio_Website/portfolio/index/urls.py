6597e1bb8f32d5a7851caf960b9ee3411781120c
merinjo90 <merinjo90@gmail.com>
2022-03-02 13:07:06 +0530

    random image collection source unsplash

--- 4_Portfolio_Website/portfolio/index/urls.py
+++ 4_Portfolio_Website/portfolio/index/urls.py
@@ -5,7 +5,7 @@ from .views import *
 
 urlpatterns = [
     path('',home,name='home'),
-    # path('home', home, name='home'),
+    path('home', home, name='home'),
     path('about',about,name='about'),
     path('project', project, name='project'),
     path('contact', contact, name='contact'),
