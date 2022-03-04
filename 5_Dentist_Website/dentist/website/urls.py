761f060d07a741e59e843a6e244cbd71d65756b1
merinjo90 <merinjo90@gmail.com>
2022-03-04 12:13:46 +0530

    add other pages and urls

--- 5_Dentist_Website/dentist/website/urls.py
+++ 5_Dentist_Website/dentist/website/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('',home,name="home")
+    path('',home,name="home"),
+    path('about',about,name='about'),
+    path('service', service, name='service'),
+    path('pricing', pricing, name='pricing'),
+    path('blog', blog, name='blog'),
+    path('blogdetails', blog_details, name='blog_details'),
+    path('contact', contact, name='contact')
 
 ]
